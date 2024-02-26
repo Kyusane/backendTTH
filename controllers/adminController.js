0752d3a5fc1f5226b48edd3ff60aa94fbe0ebb9f
Rahmat Rohmani <rahmatrohmani21@gmail.com>
2024-02-26 08:05:04 +0700

    fix

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,8 +3,8 @@ const db = require("../connection")
 const getDeviceAll = (req, res) => {
      const sql = "SELECT users.username, users.email, devices.device_name FROM devices INNER JOIN users ON devices.user_id = users.id"
      try {
-          if (err) throw err
           db.query(sql, (err, fields) => {
+               if (err) throw err
                res.status(200).json(fields)
           })
      } catch (error) {
